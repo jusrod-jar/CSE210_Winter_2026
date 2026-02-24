06b504003e71b649c5dcf9fa6172603f6fcbdfe5
Justin Rodriguez <justinrodriguez0107@hotmail.com>
2026-02-24 09:53:18 -0700

    prep 1 complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,20 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("\nWhat is your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.WriteLine(firstName);
+
+        Console.Write("What is your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}");
     }
 }
