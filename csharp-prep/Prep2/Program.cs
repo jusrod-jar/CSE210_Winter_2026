665955c4661d113408e204a941302cab1e334358
Justin Rodriguez <justinrodriguez0107@hotmail.com>
2026-02-25 21:14:45 -0700

    prep 2 and 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.WriteLine("What is your grade: ");
+        string sgrade = Console.ReadLine();
+        int grade = int.Parse(sgrade);
+
+        if (grade >= 90 && grade <= 105)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80 && grade <= 89)
+        {
+            Console.WriteLine("YOu have a B");
+        }
+        else if (grade >= 70 && grade <= 79)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (grade >= 60 && grade <= 69)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+        }
+    }   
 }
