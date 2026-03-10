e3ead8a368405a314aca1ca4429e1f9d23f56e31
Justin Rodriguez <justinrodriguez0107@hotmail.com>
2026-03-09 22:53:31 -0600

    Change journal folder and finished program 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,50 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to the Journal Program!\n");
-        Journal journal = new Journal();
-        string numResponse = "";
-        do {
-        Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Receive Motivation");
-        Console.WriteLine("6. Quit");
-        Console.WriteLine("What would you like to do? ");
-        Console.Write("> ");
-        numResponse = Console.ReadLine();
+
+        Fraction fraction5 = new Fraction();
+        Random random = new Random();
+
+        for (int i = 0; i < 20; i++)
+        {
+        int topValue = random.Next(0, 20);
+        int bottomValue = random.Next(0, 20);
+
+        fraction5.SetTop(topValue);
+        fraction5.SetBottom(bottomValue);
+
+        Console.WriteLine($"Fraction: {i + 1} String: {fraction5.GetFractionString()} Decimal: {fraction5.GetDecimalValue():F3}");
+        }
+          
+        // This is all test code
+
+        // Fraction none = new Fraction();
+        // Fraction ftop = new Fraction(6);
+        // Fraction both = new Fraction(6, 7);
+
+        // none.SetBottom(8);
+        // int bottom = none.GetBottom();
+        // Console.WriteLine(bottom);
+
+        // none.SetTop(8);
+        // int top = none.GetTop();
+        // Console.WriteLine(top);
+
+        // Fraction fraction1 = new Fraction();
+        // Fraction fraction2 = new Fraction(5);
+        // Fraction fraction3 = new Fraction(3, 4);
+        // Fraction fraction4 = new Fraction(1, 3);
+
+        // string fract1 = fraction1.GetFractionString();
+        // double decFract1 = fraction1.GetDecimalValue();
+        // Console.WriteLine(fract1);
+        // Console.WriteLine(decFract1);
         
+        // string fract2 = fraction2.GetFractionString();
+        // double decFract2 = fraction2.GetDecimalValue();
+        // Console.WriteLine(fract2);
+        // Console.WriteLine(decFract2);
 
-        if (numResponse == "1")
-            {
-                Console.WriteLine();
-                journal.WriteEntry();
-            }
+        // string fract3 = fraction3.GetFractionString();
+        // double decFract3 = fraction3.GetDecimalValue();
+        // Console.WriteLine(fract3);
+        // Console.WriteLine(decFract3);
 
-        if (numResponse == "2")
-            {
-                Console.WriteLine();
-                journal.DisplayJournal();
-            }
-        if (numResponse == "3")
-            {
-                Console.WriteLine();
-                journal.LoadJournal();
-            }
-        if (numResponse == "4")
-            {
-                Console.WriteLine();
-                journal.SaveJournal();
-            }
-        if (numResponse == "5")
-            {
-                Console.WriteLine();
-                journal.Motivation();
-            }
-        } while (numResponse != "6");
+        // string fract4 = fraction4.GetFractionString();
+        // double decFract4 = fraction4.GetDecimalValue();
+        // Console.WriteLine(fract4);
+        // Console.WriteLine(decFract4);
 
-        Console.WriteLine("\nThank you for using the journal program.\n");
     }
 }
